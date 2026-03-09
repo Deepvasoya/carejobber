0540a0b4e866e5eadfef1541893b81ae91458731
Deep Vasoya <125170789+deepvasoya1999@users.noreply.github.com>
2026-03-09 08:58:29 +0530

    Fix issue

--- app/Http/Requests/ProfileSummaryFormRequest.php
+++ app/Http/Requests/ProfileSummaryFormRequest.php
@@ -8,6 +8,16 @@ class ProfileSummaryFormRequest extends Request
 {
 
     /**
+     * Prepare the data for validation (normalize Summary -> summary for case-insensitive form handling).
+     */
+    protected function prepareForValidation()
+    {
+        if ($this->has('Summary') && !$this->has('summary')) {
+            $this->merge(['summary' => $this->input('Summary')]);
+        }
+    }
+
+    /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
