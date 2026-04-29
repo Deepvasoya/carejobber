c278631d939a7fac43862916e00e1cdcb9458507
Deepvasoya <deep.patel8678@gmail.com>
2026-04-29 04:12:21 +0000

    from server

--- resources/views/cms/cms_page.blade.php
+++ resources/views/cms/cms_page.blade.php
@@ -1,38 +1,20 @@
 @extends('layouts.app')
 @section('content')
-
-@php
-    $fullWidthSlugs = ['employer-zone'];
-    $currentSlug = request()->segment(count(request()->segments()));
-    $isFullWidth = in_array($currentSlug, $fullWidthSlugs);
-@endphp
-
 <!-- Header start -->
 @include('includes.header')
-<!-- Header end -->
+<!-- Header end --> 
+<!-- Inner Page Title start -->
+@include('includes.inner_top_search')
+<!-- Inner Page Title end -->
+<div class="about-wraper">
+    <div class="container">
 
-@if(!$isFullWidth)
-    <!-- Inner Page Title start -->
-    @include('includes.inner_top_search')
-    <!-- Inner Page Title end -->
-    <div class="about-wraper">
-        <div class="container">
-            <h1>{{$cmsContent->page_title}}</h1>
-            {!! $cmsContent->page_content !!}
-        </div>
-    </div>
-@else
-    <style>
-        body .page-content > *:not(.medojob-fullwidth-wrapper):not(footer):not(header):not(#page-loader) { display: none !important; }
-        .medojob-fullwidth-wrapper { margin: 0 !important; padding: 0 !important; width: 100% !important; max-width: 100% !important; }
-        .medojob-fullwidth-wrapper > * { margin: 0 !important; }
-        body header + .medojob-fullwidth-wrapper,
-        body .header + .medojob-fullwidth-wrapper { margin-top: 0 !important; padding-top: 0 !important; }
-    </style>
-    <div class="medojob-fullwidth-wrapper" style="margin: 0 !important; padding: 0 !important; width: 100% !important;">
-        {!! $cmsContent->page_content !!}
-    </div>
-@endif
-
+        
+    <h1>{{$cmsContent->page_title}}</h1>
+    <p>{!! $cmsContent->page_content !!}</p>
+            
+       
+    </div>  
+</div>
 @include('includes.footer')
 @endsection
--- resources/views/job/inc/job.blade.php
+++ resources/views/job/inc/job.blade.php
@@ -72,7 +72,7 @@
     </div>
 	
 	
-    <div class="col-md-12">
+    <div class="col-md-6">
         <div class="formrow {!! APFrmErrHelp::hasError($errors, 'skills') !!}">
         <label for="">Skills</label>    
             <?php
